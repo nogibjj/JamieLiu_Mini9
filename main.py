abc9b3f50975fe08da2285e268ad01cd61184691
jamie <liujamie23@gmail.com>
2024-09-15 00:58:07 -0400

    test

--- main.py
+++ main.py
@@ -2,18 +2,49 @@
 Main cli or app entry point
 """
 
-from mylib.calculator import add
-import click
+from mylib.lib import *
 
-#var=1;var=2
+def g_describe():
+    g = load_dataset()
+    return g.describe()
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+
+def general_viz_combined(general_df, jupyter_render):
+    """saves all the figures at once"""
+    bar_visual(general_df, 'incidents_85_99', jupyter_render)
+    hist_visual(general_df, 'incidents_85_99', jupyter_render)
+    bar_visual(general_df, 'fatal_accidents_85_99', jupyter_render)
+    hist_visual(general_df, 'fatal_accidents_85_99', jupyter_render)
+    bar_visual(general_df, 'fatalities_85_99', jupyter_render)
+    hist_visual(general_df, 'fatalities_85_99', jupyter_render)
+    bar_visual(general_df, 'incidents_00_14', jupyter_render)
+    hist_visual(general_df, 'incidents_00_14', jupyter_render)
+    bar_visual(general_df, 'fatal_accidents_00_14', jupyter_render)
+    hist_visual(general_df, 'fatal_accidents_00_14', jupyter_render)
+    bar_visual(general_df, 'fatalities_00_14', jupyter_render)
+    hist_visual(general_df, 'fatalities_00_14', jupyter_render)
+
+
+def save_to_md():
+    with open ('graph.md', 'w') as file:
+        file.write(f'# Report\n\n')
+        file.write(f'## General Description\n\n')
+        file.write(f'{g_describe()}\n\n')
+        file.write(f'## Visualizations\n\n')
+        file.write(f'### Incidents 85-99\n\n')
+        file.write(f'![Incidents 85-99](incidents_85_99.png)\n\n')
+        file.write(f'### Fatal Accidents 85-99\n\n')
+        file.write(f'![Fatal Accidents 85-99](fatal_accidents_85_99.png)\n\n')
+        file.write(f'### Fatalities 85-99\n\n')
+        file.write(f'![Fatalities 85-99](fatalities_85_99.png)\n\n')
+        file.write(f'### Incidents 00-14\n\n')
+        file.write(f'![Incidents 00-14](incidents_00_14.png)\n\n')
+        file.write(f'### Fatal Accidents 00-14\n\n')
+        file.write(f'![Fatal Accidents 00-14](fatal_accidents_00_14.png)\n\n')
+        file.write(f'### Fatalities 00-14\n\n')
+        file.write(f'![Fatalities 00-14](fatalities_00_14.png)\n\n')
 
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    print(g_describe())
+
---
+++ mylib/lib.py
@@ -0,0 +1,57 @@
+"""
+    library file
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = 'https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv'
+
+# check = pd.read_csv(dataset)
+# print(check.shape)
+
+def load_dataset():
+    df = pd.read_csv(dataset)
+    return df
+
+def process_mean(df, col):
+    return df[col].mean()
+
+def process_median(df, col):
+    return df[col].median()
+
+def process_max(df, col):
+    return df[col].max()
+
+def process_min(df, col):
+    return df[col].min()
+
+def process_std(df, col):
+    return df[col].std()
+
+def bar_visual(df, col, jupyter_render):
+    """bar graph of a column over all airlines"""
+    x = df['airline']
+    y = df[col]
+    plt.figure(figsize=(15, 6))
+    plt.bar(x, y)
+    plt.xlabel('Airlines')
+    plt.ylabel(col)
+    plt.title(f'{col} over Airlines')
+    plt.xticks(rotation=45)
+    if not jupyter_render:
+        plt.savefig(f'{col} over Airlines.png')
+    else:
+        plt.show()
+
+def hist_visual(df, col, juputer_render):
+    """histogram of a column over all airlines"""
+    plt.figure(figsize=(10, 6))
+    plt.hist(df[col])
+    plt.xlabel(f'Number of {col}')
+    plt.ylabel('Frequency')
+    plt.title(f'Frequency of {col}')
+    if not juputer_render:
+        plt.savefig(f'Frequency of {col} histogram.png')
+    else:
+        plt.show()
